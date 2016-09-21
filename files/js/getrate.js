cc6f9ea2ea35bc3330285bceb00558ac62dbf78f
sirmaestro <jordanchen99@gmail.com>
2016-09-22 01:42:54 +1200

    Fix path error 3

--- files/js/getrate.js
+++ files/js/getrate.js
@@ -1,5 +1,5 @@
 "use strict";
-var $ = require('../files/node_modules/jquery/dist/jquery.min.js');
+var $ = require('../node_modules/jquery/dist/jquery.min.js');
 var FIXER_RESPONSE = { "base": "THB", "date": "2016-09-09", "rates": { "AUD": 0.037846, "BGN": 0.049892, "BRL": 0.093243, "CAD": 0.037244, "CHF": 0.027981, "CNY": 0.19203, "CZK": 0.68932, "DKK": 0.18986, "GBP": 0.02157, "HKD": 0.22294, "HRK": 0.19091, "HUF": 7.8725, "IDR": 377.46, "ILS": 0.10789, "INR": 1.9184, "JPY": 2.9553, "KRW": 31.738, "MXN": 0.54047, "MYR": 0.1176, "NOK": 0.23589, "NZD": 0.039027, "PHP": 1.3606, "PLN": 0.11057, "RON": 0.1135, "RUB": 1.8507, "SEK": 0.24239, "SGD": 0.038979, "TRY": 0.085304, "USD": 0.028744, "ZAR": 0.41173, "EUR": 0.02551 } };
 function getrate() {
     $.ajax({
