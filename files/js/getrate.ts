dfa24c3c7a3e0bf0b11f06cec5379090e4c2c93b
sirmaestro <jordanchen99@gmail.com>
2016-09-22 13:55:05 +1200

    Fixed addEventListener not working

--- files/js/getrate.ts
+++ files/js/getrate.ts
@@ -1,3 +1,4 @@
+import * as $ from '..\files\node_modules\jquery\dist\jquery.min';
 
 var FIXER_RESPONSE = {"base":"THB","date":"2016-09-09","rates":{"AUD":0.037846,"BGN":0.049892,"BRL":0.093243,"CAD":0.037244,"CHF":0.027981,"CNY":0.19203,"CZK":0.68932,"DKK":0.18986,"GBP":0.02157,"HKD":0.22294,"HRK":0.19091,"HUF":7.8725,"IDR":377.46,"ILS":0.10789,"INR":1.9184,"JPY":2.9553,"KRW":31.738,"MXN":0.54047,"MYR":0.1176,"NOK":0.23589,"NZD":0.039027,"PHP":1.3606,"PLN":0.11057,"RON":0.1135,"RUB":1.8507,"SEK":0.24239,"SGD":0.038979,"TRY":0.085304,"USD":0.028744,"ZAR":0.41173,"EUR":0.02551}}
 
@@ -25,8 +26,6 @@ var selectedcurrency = 'NZD';
 var selectedcurrency2 = 'USD';
 var amount = 1;
 var inputamount : HTMLInputElement = <HTMLInputElement> $("#input-amount")[0];
-var currencydropdown : HTMLInputElement = <HTMLInputElement> $("#currency-dropdown")[0];
-var currencydropdown2 : HTMLInputElement = <HTMLInputElement> $("#currency-dropdown-2")[0];
 
 inputamount.addEventListener("change", function() {
     var amount = (<HTMLInputElement>document.getElementById("input-amount")).value;
@@ -36,7 +35,7 @@ inputamount.addEventListener("change", function() {
     document.getElementById("output-amount").innerHTML = '$' + rate + '  ' + selectedcurrency2;
 });
 
-currencydropdown.addEventListener("change", function() {
+$("#currency-dropdown").change(function () {
     selectedcurrency = $("#currency-dropdown option:selected").attr("value");
     console.log(selectedcurrency);
     var rate = fx(amount).from(selectedcurrency).to(selectedcurrency2);
@@ -46,7 +45,7 @@ currencydropdown.addEventListener("change", function() {
     document.getElementById("output-amount").innerHTML = '$' + rate + '  ' + selectedcurrency2;
 });
 
-currencydropdown2.addEventListener("change", function() {
+$("#currency-dropdown-2").change(function () {
     selectedcurrency2 = $("#currency-dropdown-2 option:selected").attr("value");
     console.log(selectedcurrency2);
     var rate = fx(amount).from(selectedcurrency).to(selectedcurrency2);
